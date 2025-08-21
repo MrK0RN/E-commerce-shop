6a54d97155659d5b3138c2f6f80ae3ef970a2b4b
MrK0RN <147444432+MrK0RN@users.noreply.github.com>
2025-08-21 14:07:23 +0300

    Add form styling, PHP handling, and example HTML for the admin panel

---
+++ php/admin/assets/forms.php
@@ -0,0 +1,10 @@
+<?php 
+
+if (isset($_GET["edit_id"])){
+    $responce = pgQuery("SELECT * FROM ".$table_name." WHERE id = ".$_GET["edit_id"].";");
+} else {
+    $responce = pgQuery("SELECT * FROM ".$table_name." LIMIT 0;");
+}   
+
+var_dump(responce);
+?>
