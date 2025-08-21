18ccffc0774e81dc53d926acf0879eb0c50a8655
MrK0RN <147444432+MrK0RN@users.noreply.github.com>
2025-08-21 14:15:47 +0300

    Include database connection and set table name for contacts in forms.php

--- php/admin/assets/forms.php
+++ php/admin/assets/forms.php
@@ -1,5 +1,6 @@
 <?php 
-
+include "../../system/db.php";
+$table_name = "contacts";
 if (isset($_GET["edit_id"])){
     $responce = pgQuery("SELECT * FROM ".$table_name." WHERE id = ".$_GET["edit_id"].";");
 } else {
